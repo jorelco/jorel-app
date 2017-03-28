d11ad881efb5e17e4b2a164273771b25f13c39a0
Thomas Frost <alberstt@gmail.com>
2017-03-27 18:30:02 -0700

    Implemented Material-UI http://www.material-ui.com/

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 
+// MATERIAL UI REQUIRED
+import injectTapEventPlugin from "react-tap-event-plugin";
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+// END REQUIRED
+
 ReactDOM.render(
     <App />,
   document.getElementById( "root" ),
